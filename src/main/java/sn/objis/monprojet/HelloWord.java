6508d7ae0685f6dca5cc4cd4fdc679b55b7aa880
Djimangue DIEDHIOU <djimangue@gmail.com>
2017-05-11 17:56:00 +0000

    Ajout de Hello

--- src/main/java/sn/objis/monprojet/HelloWord.java
+++ src/main/java/sn/objis/monprojet/HelloWord.java
@@ -3,7 +3,7 @@ package sn.objis.monprojet;
 public class HelloWord {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Word!");
+		System.out.println("Hello Word...!");
 
 	}
 
